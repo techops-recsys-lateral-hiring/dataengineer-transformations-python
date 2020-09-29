933bbb9d591c06f2c82595ac81aa2555a3bcb289
Daniel Schruhl <danielschruhl@gmail.com>
2020-09-29 17:26:59 +0200

    Move to pipenv

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="data-transformations",
+    version="0.1.0",
+    author="ThoughtWorks Inc",
+    author_email="info@thoughtworks.com",
+    description="data transformations",
+    long_description="data transformations",
+    url="https://github.com/ThoughtWorksInc/data-transformations-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
