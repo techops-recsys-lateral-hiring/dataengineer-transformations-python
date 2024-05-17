2ee82a318f73a8022a7c2143b0c869df4976ccc4
Shruti chikode <58377789+shru2502@users.noreply.github.com>
2024-05-17 07:53:53 +0200

    Update citibike_ingest.py

--- jobs/citibike_ingest.py
+++ jobs/citibike_ingest.py
@@ -8,18 +8,20 @@ from data_transformations.citibike import ingest
 LOG_FILENAME = 'project.log'
 APP_NAME = "Citibike Pipeline: Ingest"
 
-def main(args):
+if __name__ == '__main__':
+    logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO)
+    logging.info(sys.argv)
 
-    if len(args) < 4:
+    if len(sys.argv) == 0:
         logging.warning("Input source and output path are required")
         sys.exit(1)
-    else:
-        input_path = args[2]
-        output_path = args[3]
 
-    spark = SparkSession.builder.appName("Citibike Transformer").getOrCreate()
-    logging.info("Application Initialized: " + spark.sparkContext.appName)
+    spark = SparkSession.builder.appName(APP_NAME).getOrCreate()
+    sc = spark.sparkContext
+    app_name = sc.appName
+    logging.info("Application Initialized: " + app_name)
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
     ingest.run(spark, input_path, output_path)
     logging.info("Application Done: " + spark.sparkContext.appName)
-
     spark.stop()
