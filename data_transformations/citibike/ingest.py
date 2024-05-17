32282478c44ffec487753297b4e73236c4d7b652
Shruti chikode <58377789+shru2502@users.noreply.github.com>
2024-05-17 07:52:30 +0200

    Update ingest.py

--- data_transformations/citibike/ingest.py
+++ data_transformations/citibike/ingest.py
@@ -9,7 +9,11 @@ def sanitize_columns(columns: List[str]) -> List[str]:
 
 
 def run(spark, ingestPath, transformationPath):
-    df = spark.read.parquet(ingestPath)
+    df = spark.read.csv(ingestPath, header=True, inferSchema=True)
+    df.show()
+    df.write.parquet(transformationPath,mode='append')
+    parquet_path = os.path.join(transformationPath, 'citibike.parquet')
+    df = df.to_parquet(parquet_path)
     df.show()
     df = bike_distance.computeDistances(spark, df)
     df.show()
