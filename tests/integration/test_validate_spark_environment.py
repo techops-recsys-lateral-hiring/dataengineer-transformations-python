ae0d3fbe64edec3605e85641ab1eec77faa9c0ab
Javi Molina <javier.m@thoughtworks.com>
2024-07-08 16:48:15 +0200

    Make the test easier to read with some extra functions

--- tests/integration/test_validate_spark_environment.py
+++ tests/integration/test_validate_spark_environment.py
@@ -11,25 +11,39 @@ def test_java_home_is_set():
 
 
 def test_java_version_is_greater_or_equal_11():
-    version_regex = re.compile(r'version "(?P<major>\d+)\.(?P<minor>\d+)\.\d+"')
-
-    java_version_output = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT).decode("utf-8")
+    java_version_output = get_java_version_output()
     print(f"\n`java -version` returned\n{java_version_output}")
 
-    version_line = next((line for line in java_version_output.splitlines() if "version" in line), None)
-
+    version_line = extract_version_line(java_version_output)
     if not version_line:
         pytest.fail("Couldn't find version information in `java -version` output.")
 
-    regex_match = version_regex.search(version_line)
-    if not regex_match:
-        pytest.fail(f"Couldn't parse Java version from {version_line} using {version_regex=}.")
-
-    actual_major_version = int(regex_match["major"])
-
-    if actual_major_version == 1:
-        actual_major_version = int(regex_match["minor"])
+    major_version = parse_major_version(version_line)
+    if major_version is None:
+        pytest.fail(f"Couldn't parse Java version from {version_line}.")
 
     expected_major_version = 11
-    assert actual_major_version >= expected_major_version, (f"Major version {actual_major_version} is not recent enough, "
+    assert major_version >= expected_major_version, (f"Major version {major_version} is not recent enough, "
                                                      f"we need at least version {expected_major_version}.")
+
+
+def get_java_version_output():
+    return subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT).decode("utf-8")
+
+
+def extract_version_line(java_version_output):
+    for line in java_version_output.splitlines():
+        if "version" in line:
+            return line
+    return None
+
+
+def parse_major_version(version_line):
+    version_regex = re.compile(r'version "(?P<major>\d+)\.(?P<minor>\d+)\.\d+"')
+    match = version_regex.search(version_line)
+    if not match:
+        return None
+    major_version = int(match.group("major"))
+    if major_version == 1:
+        major_version = int(match.group("minor"))
+    return major_version
