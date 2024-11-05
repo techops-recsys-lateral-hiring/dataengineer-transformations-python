7f23603a95af6a8c683ec8a2790f856e3f341c25
Nick Engelhardt <nick.engelhardt@thoughtworks.com>
2024-11-05 09:06:35 +0100

    Fix incorrect regex
    The previous regex wasn't working for java version 8, because that's
    reported as e.g. 'openjdk version "1.8.0_412"' and the underscore
    doesn't match regex pattern '\d+'.

--- tests/integration/test_validate_spark_environment.py
+++ tests/integration/test_validate_spark_environment.py
@@ -1,7 +1,6 @@
 import os
 import re
 import subprocess
-from typing import Optional
 
 import pytest
 
@@ -11,7 +10,7 @@ def test_java_home_is_set() -> None:
     assert java_home is not None, "Environment variable 'JAVA_HOME' is not set but is required by pySpark to work."
 
 
-def test_java_version_minimum_requirement(expected_major_version:int =11) -> None:
+def test_java_version_minimum_requirement(expected_major_version: int = 11) -> None:
     version_line = __extract_version_line(__java_version_output())
     major_version = __parse_major_version(version_line)
     assert major_version >= expected_major_version, (f"Major version {major_version} is not recent enough, "
@@ -24,19 +23,21 @@ def __java_version_output() -> str:
     return java_version
 
 
-def __extract_version_line(java_version_output:str) -> str:
+def __extract_version_line(java_version_output: str) -> str:
     version_line = next((line for line in java_version_output.splitlines() if "version" in line), None)
     if not version_line:
         pytest.fail("Couldn't find version information in `java -version` output.")
     return version_line
 
 
-def __parse_major_version(version_line:str) -> int:
-    version_regex = re.compile(r'version "(?P<major>\d+)\.(?P<minor>\d+)\.\d+"')
+def __parse_major_version(version_line: str) -> int:
+    version_regex = re.compile(r'version "(?P<major>\d+)\.(?P<minor>\d+)\.\w+"')
     match = version_regex.search(version_line)
-    if match:
+    if match is not None:
         major_version = int(match.group("major"))
-        if major_version == 1: # Java 8 reports version as 1.actual_major.actual_minor
+        if major_version == 1:
+            # we need to jump this hoop due to Java version naming conventions - it's fun:
+            # https://softwareengineering.stackexchange.com/questions/175075/why-is-java-version-1-x-referred-to-as-java-x
             major_version = int(match.group("minor"))
         return major_version
     pytest.fail(f"Couldn't parse Java version from {version_line}.")
